26fa2b84fec1b12af678f390789f95d54e08376e
a.shahimian <shahimian@gmail.com>
2019-07-12 19:49:40 +0430

    make spot ligh and creating shadow

--- libs/main.js
+++ libs/main.js
@@ -2,12 +2,12 @@ $(function () {
     var scene = new THREE.Scene();
     var camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
     var renderer = new THREE.WebGLRenderer();
-    renderer.setClearColor(0xEEEEEE);
+    renderer.setClearColor(0x000000);
     renderer.setSize(window.innerWidth, window.innerHeight);
-    var axes = new THREE.AxisHelper( 20 );
-    scene.add(axes);
+    renderer.shadowMapEnabled = true;
+    
     var planeGeometry = new THREE.PlaneGeometry(60,20,1,1);
-    var planeMaterial = new THREE.MeshBasicMaterial({
+    var planeMaterial = new THREE.MeshLambertMaterial({
         color: 0xcccccc
     });
     var plane = new THREE.Mesh(planeGeometry,planeMaterial);
@@ -15,29 +15,38 @@ $(function () {
     plane.position.x = 15;
     plane.position.y = 0;
     plane.position.z = 0;
+    plane.receiveShadow = true;
     scene.add(plane);
     var cubeGeometry = new THREE.CubeGeometry(4,4,4);
-    var cubeMaterial = new THREE.MeshBasicMaterial({
-        color: 0xff0000, wireframe: true
+    var cubeMaterial = new THREE.MeshLambertMaterial({
+        color: 0xff0000
     });
     var cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
     cube.position.x = -4;
     cube.position.y = 3;
     cube.position.z = 0;
+    cube.castShadow = true;
     scene.add(cube);
     var sphereGeometry = new THREE.SphereGeometry(4,20,20);
-    var sphereMaterial = new THREE.MeshBasicMaterial({
-        color: 0x7777ff, wireframe: true
+    var sphereMaterial = new THREE.MeshLambertMaterial({
+        color: 0x7777ff
     });
     var sphere = new THREE.Mesh(sphereGeometry,sphereMaterial);
     sphere.position.x = 20;
     sphere.position.y = 4;
     sphere.position.z = 2;
+    sphere.castShadow = true;
     scene.add(sphere);
     camera.position.x = -30;
     camera.position.y = 40;
     camera.position.z = 30;
     camera.lookAt(scene.position);
+
+    var spotLight = new THREE.SpotLight( 0xffffff );
+    spotLight.position.set(-40, 60, -10);
+    spotLight.castShadow = true;
+    scene.add(spotLight);
+
     $("#WebGL-output").append(renderer.domElement);
     renderer.render(scene, camera);
 });
